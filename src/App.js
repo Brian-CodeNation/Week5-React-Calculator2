10e558be71bce54f6d8e165bea1bb42f96a57b3e
Brian Harkins <brian.harkins@wearecodenation.com>
2023-08-31 16:35:52 +0100

    My calculator app 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { evaluate } from "mathjs";
 import './App.css';
 
 function App() {
+  const [sum, setSum] = useState("");
+  const [advanced, setAdvanced] = useState(false);
+  const buttons = ["7","8","9","C","4","5","6","*","1","2","3","/","+","0","-","="];
+  const advancedButtons = ["7","8","9","(","C","4","5","6",")","x","1","2","3","√","÷","+","0","-",".","="]
+
+  const handleClick = (string) => {
+      if(string === "="){
+        let expression = sum;
+        let changeSqrt = expression.replace("√", "sqrt");
+        let changeDivide = changeSqrt.replace("÷", "/");
+        let changeMultiply = changeDivide.replace("x", "*");
+        setSum(evaluate(changeMultiply))
+      } else if(string === "C"){
+          setSum("")
+      } else if(string === "√"){
+        setSum(sum + string + "(")
+      } else {
+          setSum(sum + string)
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={() => setAdvanced(!advanced)}>Advanced Calculator</button>
+      <h1>{sum}</h1>
+      <div className={advanced ? "advancedButtonWrap" : "buttonWrap"}>
+        {advanced ? 
+          advancedButtons.map((button, index) => {
+            return (
+              <button className={`button-${button}`} onClick={() => handleClick(button)} key={index}>{button}</button>
+            )
+          }) :
+          buttons.map((button, index) => {
+            return (
+              <button className={`button-${button}`} onClick={() => handleClick(button)} key={index}>{button}</button>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
